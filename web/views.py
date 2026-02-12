c7765422647200a556139d3bc6f2486bce9e6f82
AntonVelev21 <velevanton80@gmail.com>
2026-02-12 18:29:20 +0200

    Created templates structure and implemented base.html

--- web/views.py
+++ web/views.py
@@ -4,8 +4,8 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 def index(request: HttpRequest) -> HttpResponse:
-    ...
+    return render(request, 'web/home-page.html')
 
 
 def about(request: HttpRequest) -> HttpResponse:
-    ...
+    return render(request, 'web/about')
