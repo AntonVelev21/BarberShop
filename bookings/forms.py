fa353ef3be5987fda6e3fd724d593d2fa6cf5c63
AntonVelev21 <velevanton80@gmail.com>
2026-02-22 13:06:36 +0200

    Created Booking form validators for past dates, working hours, and barber availability.

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+
 from django.core.exceptions import ValidationError
 from django.forms.models import ModelForm
 from bookings.models import Booking
@@ -22,6 +24,41 @@ class BaseBookingForm(ModelForm):
             raise ValidationError('Please enter second name!')
         return entered_name
 
+
+    def clean_date_and_hour(self):
+        entered_date = self.cleaned_data.get('date_and_hour')
+        if entered_date and entered_date < timezone.now():
+            raise ValidationError('You cannot book a seat in the past!')
+        return entered_date
+
+
+    def clean(self):
+        cleaned_data = super().clean()
+        date_and_hour = cleaned_data.get('date_and_hour')
+        barber = cleaned_data.get('barber')
+
+        if date_and_hour and barber:
+            if date_and_hour.weekday() == 6:
+                self.add_error('date_and_hour', 'Please choose a working day. We work from Monday to Saturday.')
+
+
+            elif date_and_hour.minute not in [0, 30]:
+                self.add_error('date_and_hour', 'Please select a valid time slot (e.g. 10:00 or 10:30).')
+
+
+            elif date_and_hour.hour < 9 or date_and_hour.hour >= 20:
+                self.add_error('date_and_hour', 'Our working hours are between 09:00 and 18:00.')
+
+            else:
+                is_booked = Booking.objects.filter(
+                    date_and_hour=date_and_hour,
+                    barber=barber
+                ).exists()
+
+                if is_booked:
+                    self.add_error('date_and_hour', f'Sorry, {barber.first_name} is already booked for this time. Please choose another.')
+
+        return cleaned_data
 
 class BookingCreateForm(BaseBookingForm):
     ...
